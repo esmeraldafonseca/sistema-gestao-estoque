aa642aae6d80a7b0e097f753fc6955a4f70cb6c8
Esmeralda Fonseca <esmeralda.fonseca2003@gmail.com>
2026-07-29 22:50:03 +0100

    feat: adiciona funcionalidaddes a pagina de login

--- src/app/views/auth_views.py
+++ src/app/views/auth_views.py
@@ -1,20 +1,25 @@
 import flet as ft
+from ..models.database import get_Connection
+from passlib.hash import pbkdf2_sha256
 
 class LoginView:
     def __init__(self, page : ft.Page):
+        self.user_name = ft.TextField(label="User name")
+        self.password = ft.TextField(label="Password", password=True, can_reveal_password=True)
         self.page = page
 
     def build(self):
         self.page.controls.clear()
-        self.page.scroll = None  # <- garante que não há scroll a atrapalhar o expand
+        self.page.scroll = None  
 
         self.page.controls.append(
             ft.Container(
                 content=ft.Column([
                     ft.Text("Login", size=24, weight="bold"),
-                    ft.TextField(label="User name"),
-                    ft.TextField(label="Password", password=True, can_reveal_password=True),
-                    ft.ElevatedButton("Login")
+                    self.user_name,
+                    self.password,
+                    ft.ElevatedButton("Login", on_click=self._handle_login),
+                    ft.ElevatedButton("Cadastrar", on_click=self._handle_register)
                 ]),
                 alignment=ft.Alignment.CENTER,
                 expand=True
@@ -22,3 +27,52 @@ class LoginView:
         )
 
         self.page.update()
+
+
+    def _handle_login(self, event):
+        user = self.user_name.value.strip()
+        passw = self.password.value.strip()
+
+        if not user or not passw:
+            print("Prencha todos os campos")
+            return  
+
+        with get_Connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT password FROM usuarios
+                WHERE username = ?
+            """, (user,))
+            resultado = cursor.fetchone() #devolve uma lista
+
+            if resultado and pbkdf2_sha256.verify(passw, resultado[0]): #
+                print("Login feito com sucesso!")
+
+                #tela inicial
+
+            else:
+                print("Nome de utilizador ou senha incorreto")
+            conn.commit()
+
+    def _handle_register(self, event):
+        user = self.user_name.value.strip()
+        passw = self.password.value.strip()
+
+        if not user or not passw:
+            print("Prencha todos os campos")
+            return  
+
+        hasded_password = pbkdf2_sha256.hash(passw)
+
+        try:
+            with get_Connection() as conn:
+                cursor = conn.cursor()
+                cursor.execute("""
+                    INSERT INTO usuarios (username, password)
+                    VALUES (?, ?)
+                """, (user, hasded_password))
+                print("Usuario cadastrado com sucesso")
+                conn.commit()
+        except Exception as E:
+            print(f"Erro: {E} ao cadastrar")
+
