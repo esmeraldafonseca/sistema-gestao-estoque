783e825cdbddf8ff84a17bc336649f8b06f2ae1d
Esmeralda Fonseca <esmeralda.fonseca2003@gmail.com>
2026-08-06 23:53:38 +0100

    refactor: altera o schema da tabela produtos

--- src/app/models/database.py
+++ src/app/models/database.py
@@ -4,26 +4,20 @@ import os
 DB_NAME = "estoque.db"
 
 def get_Connection():
-    return sqlite3.Connection(DB_NAME)
+    conn = sqlite3.Connection(DB_NAME)
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn
 
 def create_table():
     with get_Connection() as conn:
         cursor = conn.cursor()
+
         #tabela usuarios
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS usuarios(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL)
-        """)
-
-        #tabela produtos
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS produtos(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            price TEXT NOT NULL,
-            quatity INTEGER)
         """)
 
         #tabela fornecedores
@@ -35,33 +29,17 @@ def create_table():
             email TEXT NOT NULL UNIQUE)
         """)
 
-        conn.commit()
-
-
-def update_products_suppliers():
-    product_supplier_pairs = [
-        (1, 3),   # fornecedor_id=1 para o produto id=3
-        (1, 4),   # fornecedor_id=1 para o produto id=4
-        (2, 5),   # fornecedor_id=2 para o produto id=5
-        (2, 6),
-    ]
-
-    with get_Connection() as conn:
-        cursor = conn.cursor()
-        cursor.executemany("""
-            UPDATE produtos
-            SET fornecedor_id = ?
-            WHERE id = ?
-        """, product_supplier_pairs)
-        conn.commit()
-
-#ADICIONA COLUNA DE FORENG KEY A TABLEA PRODUTOS
-def add_fornecedor_column():
-    with get_Connection() as conn:
-        cursor = conn.cursor()
+        #tabela produtos
         cursor.execute("""
-            ALTER TABLE produtos 
-            ADD COLUMN fornecedor_id INTEGER 
-            REFERENCES fornecedores(id)
+            CREATE TABLE IF NOT EXISTS produtos(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            price REAL NOT NULL,
+            quatity INTEGER NOT NULL,
+            fornecedor_id INTEGER NOT NULL REFERENCES fornecedores(id))
         """)
+
+
+
         conn.commit()
+
